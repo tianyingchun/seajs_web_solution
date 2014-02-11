5264081ab5c4c5e6056c92f3a62d860cee36770d
tianyingchun <tianyingchun@outlook.com>
2014-02-11 11:29:27 +0800

    update the code format

--- tools/deploy.js
+++ tools/deploy.js
@@ -10,65 +10,65 @@ var path = require("path");
 var fs = require('fs-extra');
 // var fs_extend = require("./fs_extend.js");
 program
-  .version('0.0.1')
-  .option('-p, --path [type]', 'current seajs module pwd directory!')
-  .option('-m, --module [type]', 'deploy specific module')
-  .parse(process.argv);
+	.version('0.0.1')
+	.option('-p, --path [type]', 'current seajs module pwd directory!')
+	.option('-m, --module [type]', 'deploy specific module')
+	.parse(process.argv);
 
 // from command line arguments get module name -m.
 var sea_lib_path = program.path;
 // print sea lib path.
-grunt.log.writeln("1. Sea-lib module path: `", sea_lib_path,"`");
+grunt.log.writeln("1. Sea-lib module path: `", sea_lib_path, "`");
 
-var packagePath = sea_lib_path+"/package.json";
+var packagePath = sea_lib_path + "/package.json";
 
 // create module dir.
-var createModuleDir = function (path, callback) {
-	fs.mkdirs(path, function (err) {
-	    if (err) {
-	        console.log(err);
-	    } else {
-	        grunt.log.write('5. Directory `'+path+'` created ').ok();
-	        if(callback) callback();
-	    }
+var createModuleDir = function(path, callback) {
+	fs.mkdirs(path, function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			grunt.log.write('5. Directory `' + path + '` created ').ok();
+			if (callback) callback();
+		}
 	});
 };
 // http://jprichardson.github.com/node-fs-extra/
-var copyFiles2Dir = function (target, dist) {
+var copyFiles2Dir = function(target, dist) {
 	var cwd = process.cwd();
 	// convert relative path into absolute path.
 	dist = path.join(cwd, dist);
 	// console.log("target",target);
 	// console.log("dist", dist);
-	fs.copy(target, dist, function (err) {
-		if(err) return console.error(err);
-		grunt.log.write("6. deploy to '"+dist+"' successfully! ").ok();
+	fs.copy(target, dist, function(err) {
+		if (err) return console.error(err);
+		grunt.log.write("6. deploy to '" + dist + "' successfully! ").ok();
 	});
 };
-fs.exists(packagePath, function (exist) {
-	if(exist) {
+fs.exists(packagePath, function(exist) {
+	if (exist) {
 		// get package.json, covert it into packageJson
 		var packageJson = grunt.file.readJSON(packagePath);
 		var module_name = packageJson.name;
 		var module_version = packageJson.version;
 		var module_family = packageJson.family;
-		grunt.log.write("2. Read package.json info: ", module_name, module_version,module_family, " ").ok();
+		grunt.log.write("2. Read package.json info: ", module_name, module_version, module_family, " ").ok();
 		// delete directory created by current version
-		var sea_module_path = path.join("..","sea-modules",module_family,module_name,module_version);
-		grunt.log.writeln("3. Sea module relative path:" , sea_module_path);
-		fs.exists(sea_module_path, function (exist) {
-			if(exist) {
+		var sea_module_path = path.join("..", "sea-modules", module_family, module_name, module_version);
+		grunt.log.writeln("3. Sea module relative path:", sea_module_path);
+		fs.exists(sea_module_path, function(exist) {
+			if (exist) {
 				// delete it first
-				fs.remove(sea_module_path, function (err) {
+				fs.remove(sea_module_path, function(err) {
 					if (err) throw err;
-					grunt.log.write('4. Deleted directory '+module_name+'->`'+module_version+'`successfully! ').ok();
-					createModuleDir(sea_module_path, function (){
+					grunt.log.write('4. Deleted directory ' + module_name + '->`' + module_version + '`successfully! ').ok();
+					createModuleDir(sea_module_path, function() {
 						// deploy dist files.
 						copyFiles2Dir(path.join(sea_lib_path, "dist"), sea_module_path);
 					});
 				});
 			} else {
-				createModuleDir(sea_module_path, function (){
+				createModuleDir(sea_module_path, function() {
 					// deploy dist files.
 					copyFiles2Dir(path.join(sea_lib_path, "dist"), sea_module_path);
 				});
@@ -78,4 +78,3 @@ fs.exists(packagePath, function (exist) {
 		grunt.fail.fatal("could not find the `package.json` ");
 	}
 });
-
--- tools/fs_extend.js
+++ tools/fs_extend.js
@@ -1,74 +1,72 @@
 var fs = require("fs");
- //Offers functionality similar to mkdir -p
- //Asynchronous operation. No arguments other than a
- //possible exception are given to the completion callback.
- function mkdir_p (path, mode, callback, position) {
-     mode = mode || 0777;
-     position = position || 0;
-     // make sure that the path start character isn't "/"
-     if(path.indexOf("/")==0) {
-     	path = path.substring(1);
-     }
-     parts = require('path').normalize(path).split('/');
-     if (position >= parts.length) {
-         if (callback) {
-             return callback();
-         } else {
-             return true;
-         }
-     }
- 
-     var directory = parts.slice(0, position + 1).join('/');
-    
-     fs.stat(directory, function(err) {
-         if (err === null) {
-             mkdir_p(path, mode, callback, position + 1);
-         } else {
+//Offers functionality similar to mkdir -p
+//Asynchronous operation. No arguments other than a
+//possible exception are given to the completion callback.
+function mkdir_p(path, mode, callback, position) {
+    mode = mode || 0777;
+    position = position || 0;
+    // make sure that the path start character isn't "/"
+    if (path.indexOf("/") == 0) {
+        path = path.substring(1);
+    }
+    parts = require('path').normalize(path).split('/');
+    if (position >= parts.length) {
+        if (callback) {
+            return callback();
+        } else {
+            return true;
+        }
+    }
 
-             fs.mkdir(directory, mode, function (err) {
-                 if (err) {
-                     if (callback) {
-                         return callback(err);
-                     } else {
-                         throw err;
-                     }
-                 } else {
-                     mkdir_p(path, mode, callback, position + 1);
-                 }
-             });
-         }
-     })
- }
+    var directory = parts.slice(0, position + 1).join('/');
+
+    fs.stat(directory, function(err) {
+        if (err === null) {
+            mkdir_p(path, mode, callback, position + 1);
+        } else {
+
+            fs.mkdir(directory, mode, function(err) {
+                if (err) {
+                    if (callback) {
+                        return callback(err);
+                    } else {
+                        throw err;
+                    }
+                } else {
+                    mkdir_p(path, mode, callback, position + 1);
+                }
+            });
+        }
+    })
+}
 //Polymorphic approach: If the third parameter is boolean
 //and true assume that caller wants recursive operation.
-exports.mkdir = function (path, mode, recursive, callback) {
-	if (typeof(recursive) != 'boolean') {
-         callback = recursive;
-         recursive = false;
-     }
+exports.mkdir = function(path, mode, recursive, callback) {
+    if (typeof(recursive) != 'boolean') {
+        callback = recursive;
+        recursive = false;
+    }
 
-     if (!recursive) {
-         fs.mkdir(path, mode, callback || noop);
-     } else {
-         mkdir_p(path, mode, callback || noop);
-     }
+    if (!recursive) {
+        fs.mkdir(path, mode, callback || noop);
+    } else {
+        mkdir_p(path, mode, callback || noop);
+    }
 };
 exports.deleteFolderRecursive = function(path, callback) {
     var err = "";
-    if(fs.existsSync(path) ) {
-        fs.readdirSync(path).forEach(function(file,index){
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function(file, index) {
             var curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()) { // recurse
-               deleteFolderRecursive(curPath);
+            if (fs.statSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
             } else { // delete file
-               fs.unlinkSync(curPath);
+                fs.unlinkSync(curPath);
             }
         });
         fs.rmdirSync(path);
-        if(callback) {
+        if (callback) {
             callback(err);
         }
     }
-  }; 
- 
- 
+};
