92383c4dbac783292c12f312effafd52fc390d8e
tianyingchun <tianyingchun@outlook.com>
2014-02-12 17:57:15 +0800

    delete files

--- modules/product/src/product.js
+++ modules/product/src/product.js
@@ -1,16 +1,14 @@
 define(function(require, exports, module) {
 
-	var product = {};
-	var picture = require("./picture");
-
-	product.showPictures = function() {
-		var pics = [];
-		pics.push(new picutre(1, "test pic 1", "http://www.baidu.com/",""));
-		pics.push(new picutre(2, "test pic 2", "http://www.baidu.com/",""));
-		pics.push(new picutre(3, "test pic 3", "http://www.baidu.com/",""));
-		pics.push(new picutre(4, "test pic 4", "http://www.baidu.com/",""));
-		pics.push(new picutre(5, "test pic 5", "http://www.baidu.com/",""));
-	};
-	module.exports = product;
-
+    var product = {};
+    var picture = require("./picture");
+    product.showPictures = function() {
+        var pics = [];
+        pics.push(new picutre(1, "test pic 1", "http://www.baidu.com/", ""));
+        pics.push(new picutre(2, "test pic 2", "http://www.baidu.com/", ""));
+        pics.push(new picutre(3, "test pic 3", "http://www.baidu.com/", ""));
+        pics.push(new picutre(4, "test pic 4", "http://www.baidu.com/", ""));
+        pics.push(new picutre(5, "test pic 5", "http://www.baidu.com/", ""));
+    };
+    module.exports = product;
 });
--- sea-modules/README.md
+++ sea-modules/README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # 存放 seajs、jquery 等文件, 这也是模块的部署目录- SeaJS 标准模块的目录
 # 此文件夹通常是，引入第三方开发者开发的插件库 比如jquery,seajs,等
 ### SEAJS 模块用SPM 来进行管理, 最终的项目 编译, 发布用grunt 来构建
@@ -10,4 +9,3 @@
 =======
 # 存放 seajs、jquery 等文件，这也是模块的部署目录- SeaJS 标准模块的目录
 # 此文件夹通常是，引入第三方开发者开发的插件库 比如jquery,seajs,等
->>>>>>> 1fca105... initialize project folders
